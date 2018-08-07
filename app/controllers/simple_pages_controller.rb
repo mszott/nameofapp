f8f3c6469da9419df87e8d23ebe90c48b18ab87d
Mike Szott <michaelszott@hotmail.com>
2018-08-07 09:53:01 -0400

    4.5 in progress, getting syntax error loading landing page

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,14 +1,20 @@
 class SimplePagesController < ApplicationController
 
   def landing_page
+    @products = Product.limit(3)
+  end
+
+
+  def landing_page
     @featured_product = Product.first
+    end
   end
 
   def index
   end
 
 
-end
+
 
 
 
